dc9ae4176ec9338756404f136e3d00891e575a12
NskB3 <40596952+NskB3@users.noreply.github.com>
2018-09-02 14:21:43 +0000

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+echo "Installer for UBUNTU/DEBIAN"
+apt-get install git -y
+apt-get install pmount -y
+echo "GIT and PMOUNT INSTALLED!"
+git clone https://github.com/NskB3/Un-Safe
+echo "Cloned UNSAFE from github..."
+echo "Setup Done!"
